b19d4652d1f90bc14240499549e4e6b779ebb6be
RISWAN <riswangciwang69@gmail.com>
2026-08-30 22:38:47 +0800

    Upload website ucapan

---
+++ script.js
@@ -0,0 +1,126 @@
+function bukaSurat() {
+
+    const awal = document.getElementById("halamanAwal");
+    const surat = document.getElementById("halamanSurat");
+
+    awal.classList.add("fade-out");
+
+    setTimeout(() => {
+
+        awal.style.display = "none";
+
+        surat.style.display = "block";
+
+        surat.classList.add("fade-in");
+
+        const musik = document.getElementById("musik");
+
+        musik.play();
+
+        ketikSurat();
+
+    }, 500);
+}
+
+
+function ketikSurat() {
+
+    const teks =
+        "Sebenarnya ada sesuatu yang ingin aku sampaikan kepadamu... ❤️";
+
+    const surat = document.getElementById("surat");
+
+    surat.innerHTML = "";
+
+    let i = 0;
+
+    function ketik() {
+
+        if (i < teks.length) {
+
+            surat.innerHTML += teks.charAt(i);
+
+            i++;
+
+            setTimeout(ketik, 60);
+        }
+    }
+
+    ketik();
+}
+
+
+function lanjut() {
+
+    const surat = document.getElementById("halamanSurat");
+    const halamanTiga = document.getElementById("halamanTiga");
+
+    surat.classList.add("fade-out");
+
+    setTimeout(() => {
+
+        surat.style.display = "none";
+
+        halamanTiga.style.display = "block";
+
+        halamanTiga.classList.add("fade-in");
+
+    }, 500);
+}
+
+
+function selesai() {
+
+    const halamanTiga = document.getElementById("halamanTiga");
+    const halamanAkhir = document.getElementById("halamanAkhir");
+
+    halamanTiga.classList.add("fade-out");
+
+    setTimeout(() => {
+
+        halamanTiga.style.display = "none";
+
+        halamanAkhir.style.display = "block";
+
+        halamanAkhir.classList.add("fade-in");
+
+        setInterval(buatHati, 300);
+
+    }, 500);
+}
+
+function buatHati() {
+
+    const hati = document.createElement("div");
+
+    hati.classList.add("hati-jatuh");
+
+    hati.innerHTML = "❤️";
+
+    hati.style.left = Math.random() * 100 + "vw";
+
+    hati.style.animationDuration =
+        (Math.random() * 2 + 3) + "s";
+
+    document.body.appendChild(hati);
+
+    setTimeout(() => {
+        hati.remove();
+    }, 5000);
+}
+
+for (let i = 0; i < 30; i++) {
+
+    const bintang = document.createElement("div");
+
+    bintang.classList.add("bintang");
+
+    bintang.style.left = Math.random() * 100 + "vw";
+
+    bintang.style.top = Math.random() * 100 + "vh";
+
+    bintang.style.animationDelay =
+        Math.random() * 2 + "s";
+
+    document.body.appendChild(bintang);
+}
